78cd8471092c24f03d7403270f42e289846bdf2f
mardaloescu <serban.stefanita@gmail.com>
2019-09-16 21:36:56 +0300

    Curs 1: Hello World.

---
+++ src/com/curs1/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.curs1;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java World!");
+        System.out.println("Salutari si spor studentilor Academiei Programatorilor!");
+    }
+
+}
